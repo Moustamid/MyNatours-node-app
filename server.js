1ec33ea0b358acd0723becc79c20e8e8a19ac520
Moustamid <karim1992moustamid@gmail.com>
2021-02-04 18:17:23 -0600

    creating a simple tour model

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-//- read the variables from the '.env' file and save them into the env varibles ✔
+//! read the variables from the '.env' file and save them into the env varibles ✔
 const dotenv = require('dotenv').config();
 
 const app = require('./app');
 
-//-Connection to mongoose :
+//!Connection to mongoose :
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -19,11 +19,41 @@ mongoose
     useFindAndModify: false
   })
   .then(con => {
-    console.log(con.connections);
+    // console.log(con.connections);
     console.log('DB connection successful');
   });
 
-// - start server :
+//- schema TODO: move to an other folder ... starts here .
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name '],
+    unique: true
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price '] }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  // rating: 4.7,
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log('doc', doc);
+  })
+  .catch(err => {
+    console.log('err 😨', err);
+  });
+
+//- schema TODO:  move to an other folder ... ends here .
+
+// ! start server :
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
